a9c8bc244d51e238388af31b6192a5435fd077c3
scheng <scheng2019@gmail.com>
2021-03-21 13:43:27 -0400

    Finish project

--- main.py
+++ main.py
@@ -1,13 +1,74 @@
 from util import *
 import time
-d = 16
-n = 40
-num_trials = 100
-sum_scores = 0.0
-t = time.time()
-for _ in range(num_trials):
-    ba = ImprovedAgent(d, n)
-    ba.run()
-    sum_scores += ba.score
-print('Average score: ' + str(round(sum_scores / num_trials, 3)))
-print(time.time() - t)
+import concurrent.futures
+import matplotlib.pyplot as plt
+
+
+def density_plot():
+    exec = concurrent.futures.ProcessPoolExecutor(max_workers=20)
+
+    # Plot parameters (Dimension of minesweeper board, number of trials per data point, number of densities to test)
+    dim = 30
+    num_trials = 20
+    p_steps = 20
+    bonus = True  # Comment / un comment agents within futures as well
+
+    # Submit all games to the executor
+    futures = {}
+    for _ in range(num_trials):
+        for p in range(p_steps):
+            for agent in [
+                None,                                    # -> Basic Agent
+                (-1, False),                             # -> Improved Agent
+                (round(dim * dim * p / p_steps), False)  # -> Bonus: Global Mine Information
+                # (-1, True)                             # -> Bonus: Optimized Selection Algorithm
+            ]:
+                futures[exec.submit(density_trial, dim, round(dim * dim * p / p_steps), agent)] = (p / p_steps, agent)
+
+    t = time.time()
+
+    # Data which will be plotted
+    density = [[p / p_steps + i / 200 for p in range(p_steps)] for i in range(3)]
+    results = [[0 for _ in range(p_steps)] for i in range(3)]
+
+    # Processing the results and moving them to correct agent
+    for f in concurrent.futures.as_completed(futures):
+        if futures[f][1]:
+            if futures[f][1] == (-1, False):
+                results[1][round(futures[f][0] * p_steps)] += f.result() / num_trials
+            else:
+                results[2][round(futures[f][0] * p_steps)] += f.result() / num_trials
+        else:
+            results[0][round(futures[f][0] * p_steps)] += f.result() / num_trials
+
+    print("Time: " + str(time.time() - t))
+
+    # Plot the results, Basic, Improved, and Bonus are Red, Green and Blue repsectively
+    for i in range(3 if bonus else 2):
+        plt.scatter(density[i], results[i], s=5, c=["Red", "Green", "Blue"][i])
+    plt.scatter([0, 1], [0, 1], s=0)
+    plt.title('Figure 2')
+    plt.xlabel('Mine Density (total mines / total cells)')
+    plt.ylabel('Final score (identified mines / total mines)')
+    plt.savefig('figure2.png')
+    plt.show()
+
+
+def density_trial(dimension, mines, improved):
+    # A single minesweeper game
+    if improved:
+        agent = ImprovedAgent(dimension, mines, improved)
+    else:
+        agent = BasicAgent(dimension, mines)
+    agent.run()
+    return agent.score
+
+
+def play_by_play():
+    agent = ImprovedAgent(10, 10, (-1, False, 0))
+    agent.run()
+
+
+if __name__ == "__main__":
+    density_plot()
+    play_by_play()
--- util.py
+++ util.py
@@ -158,7 +158,9 @@ def get_score(board, mines):
     for i, j in mines:
         if board[i][j] == 'M':
             num_correctly_flagged_mines += 1
-    return round(num_correctly_flagged_mines / len(mines), 2)
+    if len(mines) == 0:
+        return 1
+    return num_correctly_flagged_mines / len(mines)
 
 
 class BasicAgent:
@@ -237,10 +239,11 @@ class BasicAgent:
                     self.num_hidden_board[ni][nj] -= 1
         self.score = get_score(self.board, self.mines)
         # print_board(self.board)
-        print('Score: ' + str(self.score))
+        # print('Score: ' + str(self.score))
 
 
 class Info:
+    # This class stores information about cells we know are safe or mined
     def __init__(self, mines=None, safe=None):
         if safe is None:
             safe = set()
@@ -254,6 +257,11 @@ class Info:
             str(s) for s in self.safe)
 
     def combine(self, info):
+        """
+        Combines self with another set of information and checks for inconsistencies
+        :param info: Another Info object
+        :return: Nothing
+        """
         if len(info.mines) + len(info.safe) != len(info.mines.union(info.safe)):
             raise Exception("Some cells are both safe and mines!")
         if len(self.mines.intersection(info.safe)) != 0:
@@ -263,15 +271,28 @@ class Info:
         self.mines = self.mines.union(info.mines)
         self.safe = self.safe.union(info.safe)
 
+    def has_new(self):
+        """
+        Checks if there is new information that can be used
+        :return: True if so, False otherwise
+        """
+        return len(self.mines) != 0 or len(self.safe) != 0
+
 
 class ClueSolver:
+    # Clue represents the information around a single cell with a number
     class Clue:
         def __init__(self, num_mines, hidden_cells, mined_cells):
             self.num_mines = num_mines - mined_cells
             self.hidden_cells = hidden_cells
 
-        # TODO: Optimize this so doesn't always use dim^2 columns
         def gen_row(self, dim):
+            """
+            Generates the row representation of the equation a clue represents of the form
+            (sum a_{i * dim + j} = b) where a is 1 if cell (i, j) is hidden, 0 o/w, b is clue value
+            :param dim: Dimension of the board
+            :return: The row representing the equation
+            """
             row = [0] * (dim * dim + 1)
             for cell in self.hidden_cells:
                 row[cell[0] * dim + cell[1]] = 1
@@ -279,6 +300,13 @@ class ClueSolver:
             return row
 
         def solve(self):
+            """
+            Solves a clue.
+            If the number of mines is the same as the number of hidden cells, all cells must be mines
+            If the number of mines is zero, all cells must be safe
+            Otherwise, there is no new information
+            :return: an Info object containing any new information
+            """
             if self.num_mines == len(self.hidden_cells):
                 return Info(mines=self.hidden_cells)
             elif self.num_mines == 0:
@@ -286,33 +314,136 @@ class ClueSolver:
             else:
                 return Info()
 
-    def __init__(self, dim):
+    def __init__(self, dim, info):
         self.clues = []
         self.dim = dim
+        self.dense_to_sparse = {}
+        self.selection = info[1]
+
+    def index_to_cell(self, index):
+        """
+        Converts an index in the array to
+        :param index: The index of the particular cell in the row
+        :return: The cell
+        """
+        return index // self.dim, index % self.dim
 
     def add_clue(self, num_mines, hidden_cells, mined_cells):
+        """
+        Given the number of mines, hidden cells, and mined cells, adds clue to list of clues
+        :param num_mines: The clue number (how many mines are around it)
+        :param hidden_cells: Number of hidden cells around clue
+        :param mined_cells: Number of mined cells around clue
+        :return: Nothing, adds clue to self
+        """
+        # If there is nothing hidden, we have nothing to learn, so ignore it
         if len(hidden_cells) == 0:
             return
         self.clues.append(self.Clue(num_mines, hidden_cells, mined_cells))
 
-    def solve(self):
-        mat = []
-        for clue in self.clues:
-            mat.append(clue.gen_row(self.dim))
-        sympy_mat = sympy.Matrix(mat)
-        sympy_mat = sympy_mat.rref()[0]
+    def get_next_cell(self, sympy_mat):
+        """
+        For the bonus, picks the next best cell to use
+        Once we solve the matrix, we pick the cell included in the MOST number of constraints
+        This maximizes the amount of other clues we may be able to solve next
+        :param sympy_mat: The RREF'd sympy matrix
+        :return: The cell which is in the most number of constraints (equations)
+        """
+        max_constraint, max_cell = -1, -1
+        if self.selection:
+            for i in range(sympy_mat.cols - 1):
+                if sum(sympy_mat.col(i)) > max_constraint:
+                    max_constraint = sum(sympy_mat.col(i))
+                    max_cell = i
 
+        return (-1, -1) if max_cell == -1 else self.index_to_cell(self.dense_to_sparse[max_cell])
+
+    def generate_sympy(self):
+        """
+        Once we have all the clues, we want to combine them all to perform inference
+        This method does just that and returns the matrix which represents our knowledge base
+        :return: The matrix
+        """
+        sparse_mat, dense_mat = [], []
+        empty_col = [True] * (self.dim * self.dim) + [False]
+
+        # For each clue, add the row form of it to the matrix
+        for clue in self.clues:
+            sparse_mat.append(clue.gen_row(self.dim))
+
+        # The matrix could be very big, so we make it small by removing all columns with only 0s
+        for row in sparse_mat:
+            for i, e in enumerate(row):
+                if e != 0:
+                    empty_col[i] = False
+
+        # Track which column in the dense matrix corresponds to in the sparse matrix for conversion later
+        self.dense_to_sparse = {}
+        filled_col, cur_col = set(), 0
+        for i, e in enumerate(empty_col):
+            if not e:
+                self.dense_to_sparse[cur_col] = i
+                cur_col += 1
+                filled_col.add(i)
+        filled_col.add(self.dim * self.dim)
+
+        # Generate the dense matrix which has no column with only 0s
+        for row in sparse_mat:
+            dense_mat.append([e for i, e in enumerate(row) if i in filled_col])
+
+        # RREF matrix to simplify and obtain solutions
+        sympy_mat = sympy.Matrix(dense_mat)
+        return sympy_mat.rref()[0]
+
+    def solve_basic(self):
+        """
+        This solves the clues using the basic agent strategy.
+        :return: The solution as an Info object storing the new information
+        """
         sol = Info()
-        for row in sympy_mat.tolist():
+        for clue in self.clues:
+            sol.combine(clue.solve())
+        return sol, None
+
+    def solve_improved(self):
+        """
+        This uses the matrix form of our knowledge base after inference has been performed
+        Reverses the compression to the dense matrix, and solves the clues
+        :return: The solution, in the form of Info object, as well as the next cell to pick if needed
+        """
+        sympy_mat = self.generate_sympy()
+
+        # Use solved matrix to check for new information
+        sol = Info()
+        for sympy_row in sympy_mat.tolist():
+            # Expand row back into long form
+            row = [0] * (self.dim * self.dim + 1)
+            for i, e in enumerate(sympy_row):
+                row[self.dense_to_sparse[i]] = e
+            # Deduce the clues
             if sum([i for i in row[:-1] if i < 0]) == 0:
-                clue = self.Clue(row[-1], set((i // self.dim, i % self.dim) for i in range(len(row) - 1) if row[i] == 1), sum([i - 1 for i in row[:-1] if i > 1]))
+                clue = self.Clue(row[-1], set(self.index_to_cell(i) for i, e in enumerate(row[:-1]) if e != 0),
+                                 sum([i - 1 for i in row[:-1] if i > 1]))
                 sol.combine(clue.solve())
 
-        return sol
+        return sol, self.get_next_cell(sympy_mat)
+
+    def solve(self):
+        """
+        This attempts to solve the board. First it uses the basic algorithm. If there is no new info, it uses improved.
+        :return: The solution and the next cell to pick
+        """
+        sol, _ = self.solve_basic()
+        if sol.has_new():
+            return sol, (-1, -1)
+        else:
+            if False:
+                print("Using inference")
+            return self.solve_improved()
 
 
 class ImprovedAgent:
-    def __init__(self, arg1, arg2):
+    def __init__(self, arg1, arg2, info):
         """
         Prepare the info that improved agent needs to make inferences. This can be initialized using d, n or board,
         mines.
@@ -329,40 +460,68 @@ class ImprovedAgent:
         self.mines = mines
         self.score = 0
         self.dim = len(board)
+        self.info = info
+        self.viz = len(info) == 3
 
     def infer(self):
         """
-
-        :return: nothing
+        Iterates through the current board and collects clue to send to the clue solver
+        Then, gets the solved clues and updates the board, repeating if needed
+        :return: The next cell to pick if needed
         """
-        solver = ClueSolver(self.dim)
+        solver = ClueSolver(self.dim, self.info)
         for i in range(self.dim):
             for j in range(self.dim):
+                # If the cell is a number, we can generate a clue
                 if self.board[i][j].isdigit():
                     hidden_cells = set()
                     mined_cells = 0
                     for pair in get_neighbor_coordinates((i, j), self.dim):
                         cell = self.board[pair[0]][pair[1]]
+                        # Count mined cells, add hidden cells to list
                         if cell == 'M' or cell == 'X':
                             mined_cells += 1
                         elif cell == '?':
                             hidden_cells.add(pair)
                         elif not cell.isdigit():
                             raise Exception("Unexpected cell value: " + cell)
+                    # Add new clue to the solver
                     solver.add_clue(int(self.board[i][j]), hidden_cells, mined_cells)
-        solution = solver.solve()
-        new_clues = False
-        if len(solution.mines) != 0:
-            new_clues = True
-            for pair in solution.mines:
-                self.board[pair[0]][pair[1]] = 'M'
-        if len(solution.safe) != 0:
-            for pair in solution.safe:
-                self.board = query(pair, self.board, self.mines)
-            new_clues = True
 
-        if new_clues and not completed(self.board):
-            self.infer()
+        # Bonus: add global information clue
+        if self.info[0] != -1:
+            hidden_cells = set()
+            mined_cells = 0
+            for i in range(self.dim):
+                for j in range(self.dim):
+                    if self.board[i][j] == '?':
+                        hidden_cells.add((i, j))
+                    if self.board[i][j] == 'M' or self.board[i][j] == 'X':
+                        mined_cells += 1
+            solver.add_clue(self.info[0], hidden_cells, mined_cells)
+
+        # Solves all clues
+        solution, next_cell = solver.solve()
+        new_clues = False
+
+        # If we determine cells as mines, mark them as such
+        for pair in solution.mines:
+            self.board[pair[0]][pair[1]] = 'M'
+        # If we determine cells as safe, query them
+        for pair in solution.safe:
+            self.board = query(pair, self.board, self.mines)
+
+        # If doing play by play, print the board
+        if self.viz:
+            print("Clues")
+            print(solution)
+            print_board(self.board)
+
+        # If board has updated, infer again
+        if solution.has_new() and not completed(self.board):
+            next_cell = self.infer()
+
+        return next_cell
 
     def run(self):
         """
@@ -371,9 +530,20 @@ class ImprovedAgent:
         :return: nothing
         """
         while not completed(self.board):
-            self.infer()
+            # Infer for as long as we can
+            next_cell = self.infer()
+            # No new info able to be inferred, query a cell
             if not completed(self.board):
                 # reveal random cell and update info
-                self.board, (i, j) = random_query(self.board, self.mines)
+                if not self.info[1] or next_cell == (-1, -1):
+                    # Query a random cell
+                    self.board, (i, j) = random_query(self.board, self.mines)
+                    if self.viz:
+                        print("Forced to guess: " + str(i) + " " + str(j))
+                        print_board(self.board)
+                else:
+                    # Bonus: query the cell given by our solver as the best pick
+                    query(next_cell, self.board, self.mines)
+
         self.score = get_score(self.board, self.mines)
-        print('Score: ' + str(self.score))
+        # print('Score: ' + str(self.score))
